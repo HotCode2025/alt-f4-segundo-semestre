ac61c299a40e86fee1c15e4447e3189f9d6d58fc
Rodrigo Haro <rodri.nh@gmail.com>
2025-08-28 01:00:56 -0300

    Agrego la clase 3 de Python

--- Python/Leccion4/main.py
+++ Python/Leccion4/main.py
@@ -208,3 +208,109 @@ print(4 in tupla) # Accion booleana, su respuesta es de tipo booleana
 # En tuplas se puede convertir de tupla a lista y de lista a tupla
 
 # Clase 3 - Segundo Semestre
+
+# Repaso de set o conjunto
+# Para definir un conjunto 
+conjunto2 = set() 
+conjunto1 = {'bye', }
+conjunto2.add(7)
+conjunto2.add("Hola")
+print(conjunto2)
+conjunto1.add('Hola')
+print(conjunto1)
+print(3 not in conjunto1) # Preguntamos si el numero 3 no esta en el conjunto1
+
+# Como hacer la igualdad de dos conjuntos
+print(conjunto1 == conjunto2) # Nos devuelve como respuesta un booleano
+
+#Operaciones en conjuntos
+conjunto3 = conjunto1 | conjunto2 # La linea une los dos conjuntos
+print(conjunto3)
+
+conjunto3 = conjunto1 & conjunto2 # Que elemento tienen en comun
+print(conjunto3)
+
+conjunto3 = conjunto1 - conjunto2 # Asigna el valor que esta en el conjunto1 y no en el conjunto2
+print(conjunto3)
+conjunto3 = conjunto2 - conjunto1
+print(conjunto3)
+
+conjunto3 = conjunto1 ^ conjunto2 # Elementos que no comparten o que son diferentes entre ambos
+print(conjunto3)   
+
+conjunto3 = conjunto1 | conjunto2
+print(conjunto2.issubset(conjunto3)) # Aqui preguntamos si un conjunto es un subconjunto dentro de otro
+print(conjunto1.issubset(conjunto3)) 
+print(conjunto3.issubset(conjunto1)) 
+print(conjunto3.issubset(conjunto2)) 
+
+print(conjunto3.issuperset(conjunto1)) # Preguntamos si los elementos del conjunto1 estan dentro del 3
+print(conjunto3.issuperset(conjunto2)) # Si es verdadero quiere decir que el conjunto3 es un superconjunto
+print(conjunto2.issuperset(conjunto3))
+
+# Como saber si ambos conjuntos son disconexos, esto es si no comparten elementos en comun
+print(conjunto1.isdisjoint(conjunto2)) # No hay cosas en comun
+
+# Convertir un conjunto totalmente en inmutable
+conjunto1 = frozenset # Esto hace que el conjunto sea totalmente inmutable
+# No se puede agregar, modificar ni eliminar elementos del conjunto
+
+# Repaso Diccionarios
+diccionarioNuevo = {'Azul': 'Blue', 'Rojo': 'Red', 'Verde': 'Green', 'Amarillo': 'Yellow'}
+
+# Como eliminar
+del (diccionarioNuevo['Azul'])
+print(diccionarioNuevo)
+
+# Los diccionarios pueden almacenar diferentes tipos de datos
+diccionario2 = {'Ariel': {'Edad': 40, 'Altura': '1.83'}, 'Osvaldo': [45, 1.85], 'Natalia': [35, 1.67]}
+print(diccionario2)
+
+#Pilas usando listas
+pila = [1, 2, 3]
+
+# Agregar elementos a la pila por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+
+# Sacamos elementos desde el final
+elementoBorrado = pila.pop() #Quita el ultimo elemento y lo guarda en la variable
+print(f'Sacamos el elemento: {elementoBorrado}')
+print(f'La pila ahora queda asi: {pila}')
+
+# Sacamos otro elemento
+elementoBorrado = pila.pop()
+print(f'Sacamos el elemento: {elementoBorrado}')
+print(f'La pila ahora queda asi: {pila}')
+
+# Colas con listas
+# Estructura de datos de tipo fifo (first input / first output)
+cola = ['Ariel', 'Osvaldo', 'Liliana', 'Pilar']
+
+# Agregamos elementos al final de la cola
+cola.append('Natalia')
+cola.append('Jose')
+print(cola)
+
+# Sacamos elementos de la cola
+seRetira = cola.pop(0) 
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0) 
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0) 
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0) 
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0) 
+print(f'Atendido el cliente: {seRetira}')
+print(cola)
+
