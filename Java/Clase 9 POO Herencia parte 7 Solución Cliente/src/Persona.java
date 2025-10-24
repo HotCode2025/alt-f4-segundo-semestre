9aa64d3c7eda4a7c38ae49c8f4f29d883d858888
Gabriel Jauregui <gabijauregui10@gmail.com>
2025-10-24 18:19:05 -0300

    Herencia

---
+++ Java/Clase 9 POO Herencia parte 7 Solución Cliente/src/Persona.java
@@ -0,0 +1,28 @@
+public class Persona {
+    // Atributos comunes
+    private String nombre;
+    private char genero;
+    private int edad;
+    private String direccion;
+
+    // Constructor
+    public Persona(String nombre, char genero, int edad, String direccion) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    // Getters y setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public char getGenero() { return genero; }
+    public void setGenero(char genero) { this.genero = genero; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+}
